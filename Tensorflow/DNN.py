c3f4f6ccd74e5f440fdaf3108231b1d4c9343ae5
hakjoon <cho224621@gmail.com>
2018-11-28 11:59:02 +0900

    update keras dnn

--- Tensorflow/DNN.py
+++ Tensorflow/DNN.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-tf.set_random_seed(777)
+np.random.seed(7)
 
 xtrain = pd.read_csv('../xtrain3.csv')
-xtest = pd.read_csv('../xtest.csv')
+xtest = pd.read_csv('../xtest3.csv')
 ytest = pd.read_csv('../ytest.csv')
 ytrain = pd.read_csv('../ytrain.csv')
 
@@ -28,37 +29,35 @@ ytrain = pd.read_csv('../ytrain.csv')
 #
 # ytest = ytest.drop('PassengerId', axis=1)
 
-xtrain = xtrain.values
-ytrain = ytrain.values
-xtrain = tf.constant(xtrain)
-ytrain = tf.constant(ytrain)
+# xtrain = xtrain.values
+# ytrain = ytrain.values
+# xtrain = tf.constant(xtrain)
+# ytrain = tf.constant(ytrain)
 
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Dense(7, input_shape=(7,), activation='relu'))
+# model.add(tf.keras.layers.Dense(7, input_shape=(7,), activation='relu'))
+# model.add(tf.keras.layers.Dense(7, input_shape=(7,), activation='relu'))
+# model.add(tf.keras.layers.Dense(7, input_shape=(7,), activation='relu'))
+# model.add(tf.keras.layers.Dense(7, input_shape=(7,), activation='relu'))
+# model.add(tf.keras.layers.Dense(7, input_shape=(7,), activation='relu'))
+# model.add(tf.keras.layers.Dense(7, input_shape=(7,), activation='relu'))
+# model.add(tf.keras.layers.Dense(7, input_shape=(7,), activation='relu'))
+# model.add(tf.keras.layers.Dense(7, input_shape=(7,), activation='relu'))
+# model.add(tf.keras.layers.Dense(7, input_shape=(7,), activation='relu'))
+# model.add(tf.keras.layers.Dense(7, input_shape=(7,), activation='relu'))
+# model.add(tf.keras.layers.Dense(7, input_shape=(7,), activation='relu'))
+# model.add(tf.keras.layers.Dense(7, input_shape=(7,), activation='relu'))
+model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+model.compile(loss='mse', optimizer='Adam', metrics=['accuracy'])
 
-m = xtrain.shape[0]
-n = xtrain.shape[1]
-labels = 1
+hist = model.fit(xtrain, ytrain, validation_data=(xtest, ytest), epochs=500)
 
-X = tf.placeholder(tf.float32, [None, n])
-Y = tf.placeholder(tf.float32, [None, labels])
-
-theta1 = tf.Variable(tf.random_normal([7, 7]))
-theta2 = tf.Variable(tf.random_normal([7]))
-
-b1 = tf.Variable(tf.zeros(7))
-b2 = tf.Variable(tf.zeros(1))
-
-Z2 = tf.nn.relu(tf.matmul(X, theta1) + b1)
-hypothesis = tf.matmul(Z2, theta2) + b2
-learning_rate = 0.001
-
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
-
-avg_cost = 0
-for i in range(m):
-    c = sess.run([cost, optimizer], feed_dict={X: xtrain, Y: ytrain})
-    avg_cost += c / m
-print('m:', '%04d' % (m + 1), 'cost = ', '{:.9f}'.format(avg_cost))
+model.predict(xtest)
+plt.figure(figsize=(12, 8))
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.plot(hist.history['acc'])
+plt.plot(hist.history['val_acc'])
+plt.legend(['loss', 'val_loss', 'acc', 'val_acc'])
+plt.show()
